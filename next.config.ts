42ceba9f2cbff9c301a032541ffd551569c15e53
devFarzad <farzad@datacode.app>
2025-03-11 02:20:49 +0300

    fix some errors

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
-import createNextIntlPlugin from 'next-intl/plugin';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import createNextIntlPlugin from "next-intl/plugin";
+import withPWA from "next-pwa";
 
-const withNextIntl = createNextIntlPlugin('./i18n.ts');
+const withNextIntl = createNextIntlPlugin("./i18n.ts");
 
-const nextConfig = withNextIntl({
-  /* config options here */
-});
+// Combine the PWA plugin with the next-intl plugin
+const nextConfig = withPWA({
+  dest: "public",
+  register: false, // We're handling registration in our PWA component
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+})(withNextIntl({
+  /* Your other config options here */
+}) as any) as any;
 
 export default nextConfig;
